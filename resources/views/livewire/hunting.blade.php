c33615999ef754664e593fc5785f11274efea23f
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2024-05-15 07:06:43 -0600

    Se me olvidó subirlo

--- resources/views/livewire/hunting.blade.php
+++ resources/views/livewire/hunting.blade.php
@@ -38,7 +38,7 @@
                                     Atrapar
                                 </button>
                                 <button data-modal-hide="{{ $item->name }}-modal" type="button"
-                                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Decline</button>
+                                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cerrar</button>
                             </div>
                         </div>
                     </div>
